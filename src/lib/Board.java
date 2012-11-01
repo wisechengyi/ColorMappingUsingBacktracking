26b674ec85e1a7069bebb4c68390aaeb2a48a4cb
yi cheng <wisechengyi@gmail.com>
2012-11-01 02:40:46 -0500

    connections are correct this time.

--- src/lib/Board.java
+++ src/lib/Board.java
@@ -1,6 +1,7 @@
 package lib;
 
 import java.awt.Color;
+import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 import java.util.LinkedList;
 import java.util.ListIterator;
@@ -64,14 +65,17 @@ public class Board {
 			ListIterator<Node> it = (ListIterator<Node>) points.iterator();
 			while (it.hasNext()) {
 				Node currNode = it.next();
-				Node nearestNode = nearestUnconnectedNode(currNode);
+				Node nearestNode = nearestUnconnectedNode(currNode); // find nearest unconnected node called nearestNode
 				if (nearestNode==null)
 				{
 					continue;
 				}
-				// find nearest unconnected node called nearestNode
-				// TODO: check intersection  if no intersection with other line, connect curr with temp,
-				// bidirection
+				
+				// check intersection  if no intersection with other line, connect curr with temp,
+				if (checkIntersection(currNode, nearestNode))
+				{
+					continue;
+				}
 				
 				currNode.connect(nearestNode);
 				possibleConnectionExists=true;
@@ -83,7 +87,50 @@ public class Board {
 		}
 	
 	}
+	private boolean checkIntersection(Node nodeOne, Node nodeTwo)
+	{
+		if (nodeOne == nodeTwo) {
+			return false;
+		}
+		Point2D p1 = nodeOne.nodePosition;
+		Point2D p2 = nodeTwo.nodePosition;
+		
+		Line2D testLine = new Line2D.Float(p1,p2);
+		
+		ListIterator<Node> it = (ListIterator<Node>) points.iterator();
+		while (it.hasNext()) {
+			Node currNode = it.next();
+			if (currNode==nodeOne || currNode ==nodeTwo)
+			{
+				continue;
+			}
+			
+			Point2D p3 = currNode.nodePosition;
+			
+			for (Node otherNode : currNode.neighbors)
+			{
+				if (otherNode == nodeOne || otherNode == nodeTwo)
+				{
+					continue;
+				}
+				Point2D p4 = otherNode.nodePosition;
+				Line2D newLine = new Line2D.Float(p4,p3);
+				if (testLine.intersectsLine(newLine))
+				{
+					return true;
+				}
+			}
+
+			// if a connection is made, mark go for a second run
+//			System.out.format("%.2f %.2f\n", temp.nodePosition.getX(),
+//					nearestNode.nodePosition.getY());
+		}
+		
+		return false;
+	}
 	
+	
+
 	private Node nearestUnconnectedNode(Node currNode)
 	{
 		Node currBestChoice = null;
